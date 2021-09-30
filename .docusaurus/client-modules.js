0ebfc89abe76a75a1310002f177f0b61a875eb93
lavalse <lavalselike@gmail.com>
2021-09-30 17:21:53 +0900

    init project

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/xucong/Project/reeartn/document/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/xucong/Project/reeartn/document/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/xucong/Project/reeartn/document/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/xucong/Project/reeartn/document/src/css/custom.css"),
+];
