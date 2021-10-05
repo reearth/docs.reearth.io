84194cacba858eb4c371d19028bc9b15cbb1541d
rot1024 <aayhrot@gmail.com>
2021-10-05 16:27:25 +0900

    set up typescript and eslint

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,163 +1,165 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer/themes/github");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-(module.exports = {
-  title: 'Re:Earth Docs',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+(
+  module.exports = {
+    title: "Re:Earth Docs",
+    tagline: "Dinosaurs are cool",
+    url: "https://your-docusaurus-test-site.com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "facebook", // Usually your GitHub org/user name.
+    projectName: "docusaurus", // Usually your repo name.
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./sidebarsUserManual.js"),
+            // Please change this to your repo.
+            editUrl:
+              "https://github.com/facebook/docusaurus/edit/main/website/",
+          },
+          blog: {
+            showReadingTime: true,
+            // Please change this to your repo.
+            editUrl:
+              "https://github.com/facebook/docusaurus/edit/main/website/blog/",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
+
+    plugins: [
+      [
+        "@docusaurus/plugin-content-docs",
+        {
+          id: "userManual",
+          path: "userManual",
+          routeBasePath: "userManual",
+          sidebarPath: require.resolve("./sidebarsUserManual.js"),
+        },
+      ],
+      [
+        "@docusaurus/plugin-content-docs",
+        {
+          id: "developerGuide",
+          path: "developerGuide",
+          routeBasePath: "developerGuide",
+          sidebarPath: require.resolve("./sidebarsDeveloperGuide.js"),
+        },
+      ],
+      [
+        "@docusaurus/plugin-content-docs",
+        {
+          id: "tutorial",
+          path: "tutorial",
+          routeBasePath: "tutorial",
+          sidebarPath: require.resolve("./sidebarsTutorial.js"),
+        },
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebarsUserManual.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        navbar: {
+          title: "Re:Earth Docs",
+          logo: {
+            alt: "My Site Logo",
+            src: "img/logo.svg",
+          },
+          items: [
+            // {
+            //   type: 'doc',
+            //   docId: 'intro',
+            //   position: 'left',
+            //   label: 'User manual',
+            // },
+            {
+              to: "/userManual/getting-started/why-reearth", // ./docs-api/Intro.md
+              label: "User manual",
+              position: "left",
+              activeBaseRegex: `/userManual/`,
+            },
+            {
+              to: "/developerGuide/introduction/about", // ./docs-api/Intro.md
+              label: "Developer guide",
+              position: "left",
+              activeBaseRegex: `/developerGuide/`,
+            },
+            {
+              to: "/tutorial/index", // ./docs-api/Intro.md
+              label: "Tutorial",
+              position: "left",
+              activeBaseRegex: `/tutorial/`,
+            },
+            { to: "/blog", label: "Blog", position: "left" },
+            {
+              href: "https://github.com/facebook/docusaurus",
+              label: "GitHub",
+              position: "right",
+            },
+          ],
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "Docs",
+              items: [
+                {
+                  label: "Tutorial",
+                  to: "/docs/intro",
+                },
+              ],
+            },
+            {
+              title: "Community",
+              items: [
+                {
+                  label: "Stack Overflow",
+                  href: "https://stackoverflow.com/questions/tagged/docusaurus",
+                },
+                {
+                  label: "Discord",
+                  href: "https://discordapp.com/invite/docusaurus",
+                },
+                {
+                  label: "Twitter",
+                  href: "https://twitter.com/docusaurus",
+                },
+              ],
+            },
+            {
+              title: "More",
+              items: [
+                {
+                  label: "Blog",
+                  to: "/blog",
+                },
+                {
+                  label: "GitHub",
+                  href: "https://github.com/facebook/docusaurus",
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
         },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
         },
       }),
-    ],
-  ],
-
-  plugins: [
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: 'userManual',
-        path: 'userManual',
-        routeBasePath: 'userManual',
-        sidebarPath: require.resolve('./sidebarsUserManual.js'),
-      },
-    ],
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: 'developerGuide',
-        path: 'developerGuide',
-        routeBasePath: 'developerGuide',
-        sidebarPath: require.resolve('./sidebarsDeveloperGuide.js'),
-      },
-    ],
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: 'tutorial',
-        path: 'tutorial',
-        routeBasePath: 'tutorial',
-        sidebarPath: require.resolve('./sidebarsTutorial.js'),
-      },
-    ],
-  ],
-
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Re:Earth Docs',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          // {
-          //   type: 'doc',
-          //   docId: 'intro',
-          //   position: 'left',
-          //   label: 'User manual',
-          // },
-          {
-            to: '/userManual/getting-started/why-reearth',    // ./docs-api/Intro.md
-            label: 'User manual',
-            position: 'left',
-            activeBaseRegex: `/userManual/`,
-          },
-          {
-            to: '/developerGuide/introduction/about',    // ./docs-api/Intro.md
-            label: 'Developer guide',
-            position: 'left',
-            activeBaseRegex: `/developerGuide/`,
-          },
-          {
-            to: '/tutorial/index',    // ./docs-api/Intro.md
-            label: 'Tutorial',
-            position: 'left',
-            activeBaseRegex: `/tutorial/`,
-          },
-          { to: '/blog', label: 'Blog', position: 'left' },
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-});
+  }
+);
--- src/components/HomepageFeatures.tsx
+++ src/components/HomepageFeatures.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import clsx from "clsx";
+import React from "react";
 
-const FeatureList = [
+import styles from "./HomepageFeatures.module.css";
+
+type FeatureItem = {
+  title: string;
+  image: string;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Easy to Use",
+    image: "/img/undraw_docusaurus_mountain.svg",
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -14,8 +27,8 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    title: "Focus on What Matters",
+    image: "/img/undraw_docusaurus_tree.svg",
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -24,8 +37,8 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    title: "Powered by React",
+    image: "/img/undraw_docusaurus_react.svg",
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
@@ -35,11 +48,11 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ title, image, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
+        <img className={styles.featureSvg} alt={title} src={image} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
@@ -49,7 +62,7 @@ function Feature({Svg, title, description}) {
   );
 }
 
-export default function HomepageFeatures() {
+export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
